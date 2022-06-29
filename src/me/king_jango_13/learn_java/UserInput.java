6f67329bad3d473bebc0f621fd807db03a2f1ece
KingJango13 <97196637+KingJango13@users.noreply.github.com>
2022-06-29 18:05:39 -0400

    Update gitignore

--- src/me/king_jango_13/learn_java/UserInput.java
+++ src/me/king_jango_13/learn_java/UserInput.java
@@ -1,4 +1,17 @@
 package me.king_jango_13.learn_java;
 
+import java.io.InputStream;
+import java.util.*;
+
+@SuppressWarnings("unused")
 public class UserInput {
+    public static String inputString(InputStream inputStream){
+        Scanner scanner = new Scanner(inputStream);
+        String str = scanner.nextLine();
+        scanner.close();
+        return str;
+    }
+    public static String inputString(){
+        return inputString(System.in);
+    }
 }
